bf32e74eb80f2bf32918e5ad603c09caa7029ccc
maumau <maurgene.ghezal@gmail.com>
2017-05-18 12:47:05 +0200

    modif du script

--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-tar zxvf spark.tar.gz ~/spark
-tar zxvf scala.tar.gz ~/scala
+tar zxvf spark.tar.gz 
+tar zxvf scala.tar.gz
+
+chmod a+x ~/.bashrc
+
+source ~/.bashrc
+
+./spark/sbin/start-slave.sh spark://192.168.43.113:7077
